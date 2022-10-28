59c0df544316e74b07124e59cb8c05d5a66f9358
MoshMage <moshmage@gmail.com>
2022-10-28 19:24:30 +0100

    adds api connection and connects FE to BE via index#fetchAndSetTranslation()

---
+++ pages/api/translate.ts
@@ -0,0 +1,42 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import axios from "axios";
+
+
+/**
+ * Uses AXIOS to unmarshal a URL and get its source and will attempt to translate it
+ *
+ * 200 OK {parsedSource: string, contractSource: string}
+ * 400 NOK {message: string, error?: string}
+ */
+async function post(req: NextApiRequest, res: NextApiResponse) {
+  if (!req?.body?.url)
+    return res.status(400).json({message: `missing 'url' on body`});
+
+  try {
+    const contractSource = await axios.get<string>(new URL(req.body.url).toString()).then(({data}) => data);
+    console.log('source!', contractSource);
+
+
+
+    console.log('parsed', "not parsed yet");
+
+    return res.status(200).json({parsedSource: "", contractSource});
+
+
+  } catch (e: any) {
+    return res.status(400).json({message: 'error unmarshalling provided url', error: e?.message || e?.toString()})
+  }
+}
+
+
+export default async function Translate(req: NextApiRequest, res: NextApiResponse) {
+  switch (req?.method?.toLowerCase()) {
+    case "post":
+      await post(req, res);
+      break;
+    default:
+      res.status(405);
+  }
+
+  res.end();
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { useInView } from "react-intersection-observer";
 import Nav from "@/components/Nav";
 import ChatBubble from "@/components/ChatBubble";
-// import Translation from "../components/Translation";
+import axios from "axios";
+
 
 export default function Home() {
+
   const [bgIsVisible, setBgIsVisible] = useState(false);
 
   const { ref, inView } = useInView({
@@ -13,6 +15,29 @@ export default function Home() {
   });
 
   console.log(inView);
+  const [contractUrl, setContractUrl] = useState("");
+  const [translation, setTranslation] = useState({__html: "Translation goes here"});
+
+
+  function fetchAndSetTranslation() {
+    if (!contractUrl)
+      return console.debug(`No contractURL`);
+
+    console.log(`url`, contractUrl)
+
+    axios.post(`api/translate`, {url: contractUrl})
+      .then(({data}) => data)
+      .then(({parsedSource, contractSource}) => {
+        console.debug(`contractSource`, contractSource);
+        console.debug(`parsedSource`, parsedSource);
+        return {__html: parsedSource};
+      })
+      .then(parsedSource => setTranslation(parsedSource))
+      .catch(e => {
+        console.debug(`Error while fetching translation`, e?.message || e?.toString());
+        return 'Error fetching translation'
+      });
+  }
 
   return (
     <>
@@ -29,24 +54,15 @@ export default function Home() {
         <ChatBubble side="right" value="Smart What ?!" />
         <p>inView: ${inView}</p>
         {/* <h1>Home</h1>
-      <form>
         <input
           type="url"
           value={contractUrl}
-          onChange={(evt) => {
-            setContractUrl(evt.target.value);
-          }}
+          onChange={(evt) => { setContractUrl(evt.target.value); }}
         />
-        <button
-          onClick={() => {
-            console.log(contractUrl);
-          }}
-        >
-          Translate
-        </button>
-      </form>
+        <button type="button" onClick={() => { fetchAndSetTranslation() } }>Translate</button>
       <hr />
-      <Translation>translation goes here!</Translation> */}
+      {/* needs pre element to respect newline|return */}
+      <pre dangerouslySetInnerHTML={translation}></pre> */}
       </main>
     </>
   );
