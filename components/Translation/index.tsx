ab9330ac04d732147dbb7c5d3a5c31ae97d1afb1
MoshMage <moshmage@gmail.com>
2022-10-28 23:07:53 +0100

    removes translation elements from home, moves them to the Translation component

--- components/Translation/index.tsx
+++ components/Translation/index.tsx
@@ -1,4 +1,6 @@
-import { ReactNode } from "react";
+import {ReactNode, useState} from "react";
+import {useAccount} from "@web3modal/react";
+import axios from "axios";
 
 interface TranslationProps {
   children: ReactNode;
@@ -6,7 +8,43 @@ interface TranslationProps {
 
 const Translation = (props: TranslationProps) => {
   const { children } = props;
-  return <div>{children}</div>;
+
+  const [contractUrl, setContractUrl] = useState("");
+  const [translation, setTranslation] = useState({__html: "Translation goes here"});
+  const { account } = useAccount()
+
+  function fetchAndSetTranslation() {
+
+    if (!contractUrl)
+      return console.debug(`No contractURL`);
+
+    console.log(`url`, contractUrl)
+
+    axios.post(`api/translate`, {url: contractUrl})
+      .then(({data}) => data)
+      .then(({parsedSource, contractSource}) => {
+        console.debug(`contractSource`, contractSource);
+        console.debug(`parsedSource`, parsedSource);
+        return {__html: parsedSource};
+      })
+      .then(parsedSource => setTranslation(parsedSource))
+      .catch(e => {
+        console.debug(`Error while fetching translation`, e?.message || e?.toString());
+        return 'Error fetching translation'
+      });
+  }
+
+  return <div>
+    <input
+      type="url"
+      value={contractUrl}
+      onChange={(evt) => {setContractUrl(evt.target.value);}} />
+
+    <button onClick={() => { fetchAndSetTranslation(); }}>translate</button>
+
+    <pre dangerouslySetInnerHTML={translation}></pre>
+
+  </div>;
 };
 
 export default Translation;
