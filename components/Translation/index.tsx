fe9a35992c931046894d56f40c8a79d53f86d4dd
MoshMage <moshmage@gmail.com>
2022-10-29 01:09:30 +0100

    ipfs is a thing

--- components/Translation/index.tsx
+++ components/Translation/index.tsx
@@ -1,6 +1,7 @@
 import {ReactNode, useEffect, useState} from "react";
 import {useAccount} from "@web3modal/react";
 import axios from "axios";
+import {Web3Storage} from "web3.storage";
 
 interface TranslationProps {
   children: ReactNode;
@@ -15,15 +16,13 @@ const Translation = (props: TranslationProps) => {
   const [translation, setTranslation] = useState({__html: "Translation goes here"});
   const { account } = useAccount();
   const [web3storageKey,] = useState(process.env.NEXT_PUBLIC_WEB3_STORAGE_API_KEY);
-
-  useEffect(() => { console.log('key',web3storageKey); } ,[])
+  const [web3Storage,] = useState(new Web3Storage({token: web3storageKey!}))
 
   function fetchAndSetTranslation() {
 
     if (!contractUrl)
       return console.debug(`No contractURL`);
 
-    console.log(`url`, contractUrl)
 
     axios.post(`api/translate`, {url: contractUrl})
       .then(({data}) => data)
@@ -39,16 +38,35 @@ const Translation = (props: TranslationProps) => {
       });
   }
 
-  function saveCurrentTranslation() {
+  async function saveCurrentTranslation() {
     if (!translation) {
       console.debug(`Missing translation`)
       return;
     }
 
+    let cid = null;
+    if (account?.address) {
+      const name = contractUrl.split('/').pop()
+
+      const file = new Blob([translation.__html], {type: 'text/plain'});
+      cid = await web3Storage.put([new File([file], name!)])
+    }
+
     const previous = JSON.parse(localStorage.getItem('translations') || '{}');
-    const newState = {...previous, [contractUrl]: {customName: '', translation}};
+    const newState = {...previous, [contractUrl]: {customName: '', translation: !cid ? translation.__html : null, cid}};
 
     localStorage.setItem('translations', JSON.stringify(newState));
+  }
+
+  async function retrieveContractFromIPFS(cid: string) {
+    const answer = await web3Storage.get(cid);
+    if (!answer?.ok) {
+      console.debug(`Failed to get cid ${cid}`);
+      return;
+    }
+
+    const files = await answer.files();
+    files[0].text().then(v => setTranslation({__html: v}));
   }
 
   return <div>
