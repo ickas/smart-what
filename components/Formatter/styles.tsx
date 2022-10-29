07b6788127ed02b7a2840fb5a5e2c877d8a91ed4
0x1887 <hey@henriquemacedo.email>
2022-10-29 05:17:14 +0100

    Add formatter styling

--- components/Formatter/styles.tsx
+++ components/Formatter/styles.tsx
@@ -27,6 +27,40 @@ export const Wrapper = styled.div`
     }
   }
 
+  pre {
+    margin: 0;
+    font-family: ${formatter.fontFamily};
+    font-size: ${formatter.fontSize};
+    line-height: ${formatter.lineHeight};
+    white-space: pre-wrap;
+    white-space: -moz-pre-wrap;
+    white-space: -pre-wrap;
+    white-space: -o-pre-wrap;
+    word-wrap: break-word;
+
+    ul {
+      margin: ${rem("15px")} 0 0 0;
+      padding: 0 0 0 ${rem("30px")};
+    }
+
+    span {
+      font-weight: ${typography.fontWeigthMedium};
+
+      &.color1 {
+        color: ${formatter.color1};
+      }
+
+      &.color2 {
+        color: ${formatter.color2};
+      }
+
+      &.color3 {
+        color: ${formatter.color3};
+      }
+
+      &.color4 {
+        color: ${formatter.color4};
+      }
     }
   }
 `;
