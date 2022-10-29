199f6ceaf27378eb45ee13264f562812976862ea
MoshMage <moshmage@gmail.com>
2022-10-29 14:30:27 +0100

    throw error on parseTemplate to know process.cwd

--- utils/parse-template.mjs
+++ utils/parse-template.mjs
@@ -6,7 +6,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default function parseTemplate(fileName = "", context = {}, templateDir = "") {
-  templateDir = templateDir || process.env.TEMPLATE_DIR || `${__dirname}/../utils/templates/`;
+  throw new Error(`${process.cwd()} -- ${__dirname} `)
+  templateDir = templateDir || process.env.TEMPLATE_DIR || `${__dirname || process.cwd()}/../utils/templates/`;
 
   const template = handlebars.compile(fs.readFileSync(path.resolve(path.join(templateDir, `${fileName}.hbs`)), 'utf-8'), {noEscape: true});
   return template(context);
