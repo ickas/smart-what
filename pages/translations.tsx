9c879e273fc4cf7ec2d6dd01fb744771eefc1a94
MoshMage <moshmage@gmail.com>
2022-10-29 03:07:15 +0100

    maps translations from storage to the UI

--- pages/translations.tsx
+++ pages/translations.tsx
@@ -1,13 +1,35 @@
 import Link from "next/link";
+import {useState} from "react";
 
 export default function Translations() {
+
+  const [translations, setTranslations] = useState<{file: string; value: string}[]>([]);
+
+  function fetchFromStorage() {
+    const value = JSON.parse(localStorage.getItem('translations') || '{}');
+    if (!Object.keys(value).length) {
+      console.debug(`No keys on storage`);
+      return;
+    }
+
+    setTranslations(Object.keys(value).map(file => ({file: file.split('/').pop()!, value})));
+  }
+
+  function selectTranslation(i: number) {
+    const entry = translations[i].value;
+    const value = JSON.parse(localStorage.getItem('translations')!)[entry];
+    if (value.cid) {
+      // todo connect to Translation.retrieveContractFromIPFS
+    } else {
+      // todo connect to Translation.fetchAndSetTranslation
+    }
+  }
+
   return (
     <div>
       <h1>My translations</h1>
       <ul>
-        <li>Translation 1</li>
-        <li>Translation 2</li>
-        <li>Translation 3</li>
+        {translations.map(({file}, i) => <li>{file}</li>)}
       </ul>
       <Link href="/">
         <span>Back</span>
